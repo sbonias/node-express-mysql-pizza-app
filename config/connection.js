8f270827d744a9847a07976e5546fd733e8525d2
Sam Bonias <sbonias84@gmail.com>
2020-06-28 20:26:32 -0400

    updated package.json

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSBD_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
-    database: "pizza_db",
+    database: "cewvxnorokyi8kvt",
   });
 }
 
